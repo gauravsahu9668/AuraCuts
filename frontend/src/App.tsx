405cd5bdf94bc6e41102c7dc37a0e42f961eae8c
gauravsahu9668 <sahug6194@gmail.com>
2024-12-07 00:37:47 +0530

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import { Routes } from 'react-router-dom'
+function App() {
+  return (
+    <>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <Routes>
+    </Routes>
+    </>
+  )
+}
+
+export default App
